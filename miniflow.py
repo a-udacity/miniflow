da46e607c760e0090470689d221269e2ea96c216
Aziz Hararwala <ahararwala@pillartechnology.com>
2017-01-02 11:00:46 -0500

    Initial commit Stochastic Gradient Descentquiz

--- miniflow.py
+++ miniflow.py
@@ -1,99 +1,214 @@
-"""
-Write the Linear#forward method below!
-"""
+import numpy as np
 
+class Layer:
+    """
+    Base class for layers in the network.
 
-class Neuron:
-    def __init__(self, inbound_neurons=[]):
-        # Neurons from which this Node receives values
-        self.inbound_neurons = inbound_neurons
-        # Neurons to which this Node passes values
-        self.outbound_neurons = []
-        # A calculated value
+    Arguments:
+
+        `inbound_layers`: A list of layers with edges into this layer.
+    """
+    def __init__(self, inbound_layers=[]):
+        """
+        Layer's constructor (runs when the object is instantiated). Sets
+        properties that all layers need.
+        """
+        # A list of layers with edges into this layer.
+        self.inbound_layers = inbound_layers
+        # The eventual value of this layer. Set by running
+        # the forward() method.
         self.value = None
-        # Add this node as an outbound node on its inputs.
-        for n in self.inbound_neurons:
-            n.outbound_neurons.append(self)
+        # A list of layers that this layer outputs to.
+        self.outbound_layers = []
+        # New property! Keys are the inputs to this layer and
+        # their values are the partials of this layer with
+        # respect to that input.
+        self.gradients = {}
+        # Sets this layer as an outbound layer for all of
+        # this layer's inputs.
+        for layer in inbound_layers:
+            layer.outbound_layers.append(self)
 
-    # These will be implemented in a subclass.
-    def forward(self):
+    def forward():
         """
-        Forward propagation.
-
-        Compute the output value based on `inbound_neurons` and
-        store the result in self.value.
+        Every layer that uses this class as a base class will
+        need to define its own `forward` method.
         """
+        raise NotImplementedError
+
+    def backward():
+        """
+        Every layer that uses this class as a base class will
+        need to define its own `backward` method.
+        """
+        raise NotImplementedError
 
 
-class Input(Neuron):
+class Input(Layer):
+    """
+    A generic input into the network.
+    """
     def __init__(self):
-        # An Input Neuron has no inbound neurons,
-        # so no need to pass anything to the Neuron instantiator
-        Neuron.__init__(self)
-
-        # NOTE: Input Neuron is the only Neuron where the value
-        # may be passed as an argument to forward().
+        # The base class constructor has to run to set all
+        # the properties here.
         #
-        # All other Neuron implementations should get the value
-        # of the previous neurons from self.inbound_neurons
-        #
-        # Example:
-        # val0 = self.inbound_neurons[0].value
+        # The most important property on an Input is value.
+        # self.value is set during `topological_sort` later.
+        Layer.__init__(self)
 
-    def forward(self, value=None):
-        # Overwrite the value if one is passed in.
-        if value is not None:
-            self.value = value
+    def forward(self):
+        # Do nothing because nothing is calculated.
+        pass
 
+    def backward(self):
+        # An Input layer has no inputs so the gradient (derivative)
+        # is zero.
+        # The key, `self`, is reference to this object.
+        self.gradients = {self: 0}
+        # Weights and bias may be inputs, so you need to sum
+        # the gradient from output gradients.
+        for n in self.outbound_layers:
+            self.gradients[self] += n.gradients[self]
 
-class Linear(Neuron):
-    def __init__(self, inputs, weights, bias):
-        Neuron.__init__(self, inputs)
-
-        # NOTE: The weights and bias properties here are not
-        # numbers, but rather references to other neurons.
-        # The weight and bias values are stored within the
-        # respective neurons.
-        self.weights = weights
-        self.bias = bias
+class Linear(Layer):
+    """
+    Represents a layer that performs a linear transform.
+    """
+    def __init__(self, X, W, b):
+        # The base class (Layer) constructor. Weights and bias
+        # are treated like inbound layers.
+        Layer.__init__(self, [X, W, b])
 
     def forward(self):
         """
-        Set self.value to the value of the linear function output.
-
-        Your code goes here!
+        Performs the math behind a linear transform.
         """
-        self.value = self.bias.value
-        for w, x in zip(self.weights, self.inbound_neurons):
-            self.value += w.value * x.value
+        X = self.inbound_layers[0].value
+        W = self.inbound_layers[1].value
+        b = self.inbound_layers[2].value
+        self.value = np.dot(X, W) + b
+
+    def backward(self):
+        """
+        Calculates the gradient based on the output values.
+        """
+        # Initialize a partial for each of the inbound_layers.
+        self.gradients = {n: np.zeros_like(n.value) for n in self.inbound_layers}
+        # Cycle through the outputs. The gradient will change depending
+        # on each output, so the gradients are summed over all outputs.
+        for n in self.outbound_layers:
+            # Get the partial of the cost with respect to this layer.
+            grad_cost = n.gradients[self]
+            # Set the partial of the loss with respect to this layer's inputs.
+            self.gradients[self.inbound_layers[0]] += np.dot(grad_cost, self.inbound_layers[1].value.T)
+            # Set the partial of the loss with respect to this layer's weights.
+            self.gradients[self.inbound_layers[1]] += np.dot(self.inbound_layers[0].value.T, grad_cost)
+            # Set the partial of the loss with respect to this layer's bias.
+            self.gradients[self.inbound_layers[2]] += np.sum(grad_cost, axis=0, keepdims=False)
+
+
+class Sigmoid(Layer):
+    """
+    Represents a layer that performs the sigmoid activation function.
+    """
+    def __init__(self, layer):
+        # The base class constructor.
+        Layer.__init__(self, [layer])
+
+    def _sigmoid(self, x):
+        """
+        This method is separate from `forward` because it
+        will be used with `backward` as well.
+
+        `x`: A numpy array-like object.
+        """
+        return 1. / (1. + np.exp(-x))
+
+    def forward(self):
+        """
+        Perform the sigmoid function and set the value.
+        """
+        input_value = self.inbound_layers[0].value
+        self.value = self._sigmoid(input_value)
+
+    def backward(self):
+        """
+        Calculates the gradient using the derivative of
+        the sigmoid function.
+        """
+        # Initialize the gradients to 0.
+        self.gradients = {n: np.zeros_like(n.value) for n in self.inbound_layers}
+        # Sum the partial with respect to the input over all the outputs.
+        for n in self.outbound_layers:
+            grad_cost = n.gradients[self]
+            sigmoid = self.value
+            self.gradients[self.inbound_layers[0]] += sigmoid * (1 - sigmoid) * grad_cost
+
+
+class MSE(Layer):
+    def __init__(self, y, a):
+        """
+        The mean squared error cost function.
+        Should be used as the last layer for a network.
+        """
+        # Call the base class' constructor.
+        Layer.__init__(self, [y, a])
+
+    def forward(self):
+        """
+        Calculates the mean squared error.
+        """
+        # NOTE: We reshape these to avoid possible matrix/vector broadcast
+        # errors.
+        #
+        # For example, if we subtract an array of shape (3,) from an array of shape
+        # (3,1) we get an array of shape(3,3) as the result when we want
+        # an array of shape (3,1) instead.
+        #
+        # Making both arrays (3,1) insures the result is (3,1) and does
+        # an elementwise subtraction as expected.
+        y = self.inbound_layers[0].value.reshape(-1, 1)
+        a = self.inbound_layers[1].value.reshape(-1, 1)
+
+        self.m = self.inbound_layers[0].value.shape[0]
+        # Save the computed output for backward.
+        self.diff = y - a
+        self.value = np.mean(self.diff**2)
+
+    def backward(self):
+        """
+        Calculates the gradient of the cost.
+        """
+        self.gradients[self.inbound_layers[0]] = (2 / self.m) * self.diff
+        self.gradients[self.inbound_layers[1]] = (-2 / self.m) * self.diff
 
 
 def topological_sort(feed_dict):
     """
-    Sort the neurons in topological order using Kahn's Algorithm.
+    Sort the layers in topological order using Kahn's Algorithm.
 
-    `feed_dict`: A dictionary where the key is a `Input` Neuron and the value is the respective value feed to that Neuron.
+    `feed_dict`: A dictionary where the key is a `Input` Layer and the value is the respective value feed to that Layer.
 
-    Returns a list of sorted neurons.
+    Returns a list of sorted layers.
     """
 
-    input_neurons = [n for n in feed_dict.keys()]
+    input_layers = [n for n in feed_dict.keys()]
 
     G = {}
-    neurons = [n for n in input_neurons]
-    while len(neurons) > 0:
-        n = neurons.pop(0)
+    layers = [n for n in input_layers]
+    while len(layers) > 0:
+        n = layers.pop(0)
         if n not in G:
             G[n] = {'in': set(), 'out': set()}
-        for m in n.outbound_neurons:
+        for m in n.outbound_layers:
             if m not in G:
                 G[m] = {'in': set(), 'out': set()}
             G[n]['out'].add(m)
             G[m]['in'].add(n)
-            neurons.append(m)
+            layers.append(m)
 
     L = []
-    S = set(input_neurons)
+    S = set(input_layers)
     while len(S) > 0:
         n = S.pop()
 
@@ -101,7 +216,7 @@ def topological_sort(feed_dict):
             n.value = feed_dict[n]
 
         L.append(n)
-        for m in n.outbound_neurons:
+        for m in n.outbound_layers:
             G[n]['out'].remove(m)
             G[m]['in'].remove(n)
             # if no other incoming edges add to S
@@ -110,19 +225,36 @@ def topological_sort(feed_dict):
     return L
 
 
-def forward_pass(output_Neuron, sorted_neurons):
+def forward_and_backward(graph):
     """
-    Performs a forward pass through a list of sorted neurons.
+    Performs a forward pass and a backward pass through a list of sorted Layers.
 
     Arguments:
 
-        `output_Neuron`: A Neuron in the graph, should be the output Neuron (have no outgoing edges).
-        `sorted_neurons`: a topologically sorted list of neurons.
-
-    Returns the output Neuron's value
+        `graph`: The result of calling `topological_sort`.
     """
-
-    for n in sorted_neurons:
+    # Forward pass
+    for n in graph:
         n.forward()
 
-    return output_Neuron.value
+    # Backward pass
+    # see: https://docs.python.org/2.3/whatsnew/section-slices.html
+    for n in graph[::-1]:
+        n.backward()
+
+
+def sgd_update(trainables, learning_rate=1e-2):
+    """
+    Updates the value of each trainable with SGD.
+
+    Arguments:
+
+        `trainables`: A list of `Input` Layers representing weights/biases.
+        `learning_rate`: The learning rate.
+    """
+    # TODO: update all the `trainables` with SGD
+    # You can access and assign the value of a trainable with `value` attribute.
+    # Example:
+    # for t in trainables:
+    #   t.value = your implementation here
+    pass
--- nn.py
+++ nn.py
@@ -1,26 +1,81 @@
+"""
+Check out the new network architecture and dataset!
+
+Notice that the weights and biases are
+generated randomly.
+
+No need to change anything, but feel free to tweak
+to test your network, play around with the epochs, batch size, etc!
+"""
+
+import numpy as np
+from sklearn.datasets import load_boston
+from sklearn.utils import shuffle, resample
 from miniflow import *
 
-x, y, z = Input(), Input(), Input()
-inputs = [x, y, z]
+# Load data
+data = load_boston()
+X_ = data['data']
+y_ = data['target']
 
-weight_x, weight_y, weight_z = Input(), Input(), Input()
-weights = [weight_x, weight_y, weight_z]
+# Normalize data
+X_ = (X_ - np.mean(X_, axis=0)) / np.std(X_, axis=0)
 
-bias = Input()
+n_features = X_.shape[1]
+n_hidden = 10
+W1_ = np.random.randn(n_features, n_hidden)
+b1_ = np.zeros(n_hidden)
+W2_ = np.random.randn(n_hidden, 1)
+b2_ = np.zeros(1)
 
-f = Linear(inputs, weights, bias)
+# Neural network
+X, y = Input(), Input()
+W1, b1 = Input(), Input()
+W2, b2 = Input(), Input()
+
+l1 = Linear(X, W1, b1)
+s1 = Sigmoid(l1)
+l2 = Linear(s1, W2, b2)
+cost = MSE(y, l2)
 
 feed_dict = {
-	x: 6,
-	y: 14,
-	z: 3,
-	weight_x: 0.5,
-	weight_y: 0.25,
-	weight_z: 1.4,
-	bias: 2
+    X: X_,
+    y: y_,
+    W1: W1_,
+    b1: b1_,
+    W2: W2_,
+    b2: b2_
 }
 
-graph = topological_sort(feed_dict)
-output = forward_pass(f, graph)
+epochs = 10
+# Total number of examples
+m = X_.shape[0]
+batch_size = 11
+steps_per_epoch = m // batch_size
 
-print(output) # should be 12.7 with this example
+graph = topological_sort(feed_dict)
+trainables = [W1, b1, W2, b2]
+
+print("Total number of examples = {}".format(m))
+
+# Step 4
+for i in range(epochs):
+    loss = 0
+    for j in range(steps_per_epoch):
+        # Step 1
+        # Randomly sample a batch of examples
+        X_batch, y_batch = resample(X_, y_, n_samples=batch_size)
+
+        # Reset value of X and y Inputs
+        X.value = X_batch
+        y.value = y_batch
+
+        # Step 2
+        forward_and_backward(graph)
+
+        # Step 3
+        sgd_update(trainables)
+
+        loss += graph[-1].value
+
+    print("Epoch: {}, Loss: {:.3f}".format(i+1, loss/steps_per_epoch))
